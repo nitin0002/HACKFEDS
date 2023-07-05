7ce51c41d1aba7733eaae81daa8c6c6fe60e665e
Ujjawal kumar <ujjawalkr67@gmail.com>
2023-07-05 20:16:18 +0530

    responsiveness of buttons

--- components/SectionLogo.js
+++ components/SectionLogo.js
@@ -13,23 +13,25 @@ const SectionLogo = () => {
       >
         HACKFED
       </h2>
-      <h3 className="pl-px text-base font-medium leading-7 antialiased tracking-tight font-sans  text-slate-200 capitalize sm:text-lg">
+      <h3 className="pl-px m-2 text-base font-medium leading-7 antialiased tracking-tight font-sans  text-slate-200 capitalize sm:text-lg">
         HACKFED is a community of passionate developers who shares a common
         purpose
       </h3>
 
 
-      <div className="flex flex-row space-x-4 mt-8 ">
-        <a href="https://discord.gg/Rw97jUCn6k" target="_blank">
-        <button className=" flex py-2 px-4 bg-[#6666F2] text-white font-semibold rounded-lg shadow-md  focus:outline-none focus:ring-2 focus:ring-[#8814a1] focus:ring-opacity-75">
-            <FaDiscord className="h-12 w-12"/> 
+      <div className="flex flex-col  sm:flex-row space-x-4 mt-8 justify-items-center sm:justify-items-start">
+        <a href="https://discord.gg/Rw97jUCn6k" target="_blank" className="justify-items-center pl-16 sm:pl-2">
+        <button className=" flex my-4 sm:my-0 py-2 px-4 bg-[#6666F2] text-white font-semibold rounded-lg shadow-md  focus:outline-none focus:ring-2 focus:ring-[#8814a1] focus:ring-opacity-75">
+            <FaDiscord className="h-10 w-10"/> 
             <span className="py-2 px-4 text-base sm:text-xl">Join Discord</span>
         </button>
         </a>
+        <a href ="#" >
         <button className=" flex py-2 px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-semibold rounded-lg shadow-md hover:from-pink-500 hover:to-yellow-500 focus:outline-none focus:ring-2 focus:ring-ring-[#8814a1] focus:ring-opacity-20">
-            <SiSololearn className="h-12 w-12"/> 
+            <SiSololearn className="h-10 w-10"/> 
             <span className="py-2 px-4 text-base sm:text-xl">Register for Hackfed 1.0</span>
         </button>
+        </a>
       </div>
 
     </div>
